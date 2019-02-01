c6136a31e777935f68ef5a0d21932c393887f029
KowalczykL <kowalczykl@protonmail.com>
2019-02-01 15:45:56 +0100

    data formatting fun

--- node ex library/express-locallibrary-tutorial/models/author.js
+++ node ex library/express-locallibrary-tutorial/models/author.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var moment = require('moment');
 
 var Schema = mongoose.Schema;
 
@@ -33,9 +34,10 @@ AuthorSchema
 });
 
 AuthorSchema
-.virtual('date_of_death_formatted')
+.virtual('lifespan_formatted')
 .get(function () {
-  return this.date_of_birth ? moment(this.date_of_birth).format('YYYY-MM-DD') : '';
+  return '[[' + (this.date_of_birth ? moment(this.date_of_birth).format('YYYY-MM-DD') : '...') +' - ' + (this.date_of_death ? moment(this.date_of_death).format('YYYY-MM-DD') : '...') + ']]';
+  //return 'l_s_test' + this.date_of_birth ? moment(this.date_of_birth).format('YYYY-MM-DD') : '';
 });
 
 //Export model
